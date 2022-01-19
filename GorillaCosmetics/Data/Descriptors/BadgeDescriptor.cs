45aacc12ded9e6d24e97f98516a3d2aeb1407e5d
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2022-01-19 16:59:42 -0500

    a

---
+++ GorillaCosmetics/Data/Descriptors/BadgeDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+namespace GorillaCosmetics.Data.Descriptors
+{
+    public class BadgeDescriptor : MonoBehaviour
+    {
+        public string BadgeName = "Badge";
+        public string AuthorName = "Author";
+        public string Description = string.Empty;
+        public bool CustomColors = false;
+        public bool DisablePublicLobbies = false;
+        public AttachPoint attachPoint = AttachPoint.Chest;
+    }
+    public enum AttachPoint
+    {
+        RightHand,
+        LeftHand,
+        Chest
+    }
+}
---
+++ GorillaCosmetics/Data/Descriptors/FaceDescriptor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GorillaCosmetics.Data.Descriptors
+{
+    public class FaceDescriptor : MonoBehaviour
+    {
+        public string FaceName = "Face";
+        public string AuthorName = "Author";
+        public string Description = string.Empty;
+        public bool CustomColors = false;
+        public bool DisablePublicLobbies = false;
+    }
+}
